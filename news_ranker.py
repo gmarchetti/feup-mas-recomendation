3e30e4ce83775dda1b2988eb560ae1827d5c866d
Guilherme Marcetti <guilherme.marchetti@gmail.com>
2024-12-06 19:47:27 +0000

    initial integration with recommenders framework

--- news_ranker.py
+++ news_ranker.py
@@ -1,12 +1,22 @@
 import logging
-import recommenders.mind as mind
+import recommenders.datasets.mind as mind
+import os
 
 from rankers.ranker_base import RankerBase
 from recommenders.news_feed_from_training import build_news_feed
+from recommenders.models.deeprec.deeprec_utils import download_deeprec_resources 
+from recommenders.models.newsrec.newsrec_utils import prepare_hparams
+from recommenders.models.newsrec.models.nrms import NRMSModel
+from recommenders.models.newsrec.models.base_model import BaseModel
+from recommenders.models.newsrec.io.mind_iterator import MINDIterator
+from recommenders.models.newsrec.newsrec_utils import get_mind_data_set
+# from recommenders.utils.notebook_utils import store_metadata
 
 logging.basicConfig(level=logging.INFO)
 
 logger = logging.getLogger(__name__)
+
+MIND_type = 'demo'
 
 def random_eval():
     user_sessions, user_click_history = mind.read_clickhistory("train", "behaviors.tsv")
@@ -24,11 +34,45 @@ def random_eval():
         logger.info(">>> Ranks for news offering")
         logger.info(ranker.eval(user_news_feed, user_id))
 
-if __name__ == '__main__':
-    random_eval()
+# if __name__ == '__main__':
+#     random_eval()
 
+data_path = f"./data/{MIND_type}"
 
+train_news_file = os.path.join(data_path, 'train', r'news.tsv')
+train_behaviors_file = os.path.join(data_path, 'train', r'behaviors.tsv')
+valid_news_file = os.path.join(data_path, 'valid', r'news.tsv')
+valid_behaviors_file = os.path.join(data_path, 'valid', r'behaviors.tsv')
+wordEmb_file = os.path.join(data_path, "utils", "embedding.npy")
+userDict_file = os.path.join(data_path, "utils", "uid2index.pkl")
+wordDict_file = os.path.join(data_path, "utils", "word_dict.pkl")
+yaml_file = os.path.join(data_path, "utils", r'nrms.yaml')
 
-        
+mind_url, mind_train_dataset, mind_dev_dataset, mind_utils = get_mind_data_set(MIND_type)
 
-       
+if not os.path.exists(train_news_file):
+    download_deeprec_resources(mind_url, os.path.join(data_path, 'train'), mind_train_dataset)
+    
+if not os.path.exists(valid_news_file):
+    download_deeprec_resources(mind_url, \
+                               os.path.join(data_path, 'valid'), mind_dev_dataset)
+if not os.path.exists(yaml_file):
+    download_deeprec_resources(r'https://recodatasets.z20.web.core.windows.net/newsrec/', \
+                               os.path.join(data_path, 'utils'), mind_utils) 
+
+epochs = 1
+seed = 42
+batch_size = 32
+
+hparams = prepare_hparams(yaml_file, 
+                          wordEmb_file=wordEmb_file,
+                          wordDict_file=wordDict_file, 
+                          userDict_file=userDict_file,
+                          batch_size=batch_size,
+                          epochs=epochs,
+                          show_step=10)
+print(hparams)
+
+iterator = MINDIterator
+model = RankerBase(hparams, iterator, seed=seed)
+print(model.run_eval(valid_news_file, valid_behaviors_file))
--- recommenders/datasets/mind.py
+++ recommenders/datasets/mind.py
@@ -10,7 +10,7 @@ import re
 from tqdm import tqdm
 from nltk.tokenize import RegexpTokenizer
 
-from recommenders.download_utils import (
+from recommenders.datasets.download_utils import (
     maybe_download,
     download_path,
     unzip_file,

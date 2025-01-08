a097271b4944091ad1fd3c45ed74677577af5b07
Guilherme Marcetti <guilherme.marchetti@gmail.com>
2025-01-08 14:21:51 +0000

    starting title concactation with news candidate title

--- model_trainer.py
+++ model_trainer.py
@@ -2,7 +2,7 @@ import evaluate
 import numpy as np
 import os
 
-from mind_dataset_loader import MindDatasetFactory
+from models.mind_dataset_loader import MindDatasetFactory
 from recommenders.models.deeprec.deeprec_utils import download_deeprec_resources 
 from recommenders.models.newsrec.newsrec_utils import get_mind_data_set
 
@@ -31,12 +31,10 @@ if not os.path.exists(valid_news_file):
 tokenizer = AutoTokenizer.from_pretrained("distilbert/distilbert-base-uncased")
 
 def preprocess_function(examples):
-    full_concat = ""
+    full_concat = examples["cand_title"].lower()
     for title in examples["hist_titles"]:
-        full_concat += title.lower() + "[SEP]"
+        full_concat += "[SEP]" + title.lower()
     
-    full_concat += examples["cand_title"].lower()
-
     return tokenizer(full_concat, truncation=True)
 
 def compute_metrics(eval_pred):
@@ -49,7 +47,7 @@ def compute_metrics(eval_pred):
 data_loader = MindDatasetFactory(train_news_file, train_behaviors_file, 0.05)
 mind = data_loader.create_dataset_object()
 
-tokenized_mind = mind.map(preprocess_function, batched=False)
+tokenized_mind = mind.map(preprocess_function, batched=False, num_proc=16)
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
 accuracy = evaluate.load("accuracy")
@@ -62,14 +60,17 @@ model = AutoModelForSequenceClassification.from_pretrained("distilbert/distilber
 training_args = TrainingArguments(
     output_dir="models/news-prediction",
     learning_rate=2e-5,
-    per_device_train_batch_size=42,
-    per_device_eval_batch_size=16,
+    per_device_train_batch_size=32,
+    per_device_eval_batch_size=32,
     num_train_epochs=5,
     weight_decay=0.01,
     eval_strategy="epoch",
     save_strategy="epoch",
     load_best_model_at_end=True,
     push_to_hub=False,
+    fp16=True,
+    optim="adamw_torch",
+    dataloader_num_workers=4
 )
 
 trainer = Trainer(
@@ -79,7 +80,7 @@ trainer = Trainer(
     eval_dataset=tokenized_mind,
     processing_class=tokenizer,
     data_collator=data_collator,
-    compute_metrics=compute_metrics,
+    compute_metrics=compute_metrics
 )
 
-trainer.train()
+trainer.train(resume_from_checkpoint=True)

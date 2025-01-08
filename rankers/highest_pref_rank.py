793a251e40d9b6635c3c683bc5155a3390cd1d0e
Guilherme Marcetti <guilherme.marchetti@gmail.com>
2025-01-08 15:38:01 +0000

    adjusted highest prefered topic ranker

--- rankers/highest_pref_rank.py
+++ rankers/highest_pref_rank.py
@@ -1,9 +1,13 @@
+import numpy
+import logging
 from rankers.ranker_base import RankerBase
 
 class HighestPref(RankerBase):
 
     def __init__(self, iterator):
         super().__init__(iterator)
+        self.__logger = logging.getLogger(__name__)
+        self.__logger.setLevel(logging.INFO)
 
     def eval_news(self, news, user_topics):
         cand_news_topic = self._data_loader.get_news_topics(news)[0]
@@ -11,36 +15,36 @@ class HighestPref(RankerBase):
             return user_topics[cand_news_topic]
         else:
             return 0
-    
-    def predict(self, impression_group, impr_index):
-        user_history = self._data_loader.get_user_history(impr_index)
-        user_topics = {}
 
-        for news_hist in user_history:
-            news_topic = self._data_loader.get_news_topics(news_hist)[0]
-            if news_topic not in user_topics.keys():
-                user_topics[news_topic] = 1
-            else:
-                user_topics[news_topic] += 1
-
-        news_predictions = []
+    def predict(self, impression_group):
+        user_history = impression_group["history_topics"]
+        self.__logger.debug(f"User history topics: {user_history}")
         
-        highest_score = -1
-        highest_score_index = -1
-        idx = 0
+        topic_scores = {}
+        
+        for history_topic_group in user_history:
+            for topic in history_topic_group:
+                current_topic_score = topic_scores.get(topic, 0)
+                topic_scores[topic] = current_topic_score + 1
 
-        for news_cand in impression_group:
-            news_score = self.eval_news(news_cand, user_topics)
-            
-            if news_score > highest_score:
-                highest_score = news_score
-                highest_score_index = idx
-            
-            idx += 1
+        self.__logger.debug(f"User history scored topics: {topic_scores}")
 
-        for idx in range(len(impression_group)):
-            news_predictions.append(0)
-            if idx == highest_score_index:
-                news_predictions[idx] = 1
+        cand_news_topics_group : list = impression_group["candidates_topics"] 
+
+        news_scores = numpy.array([])
+        
+        for cand_topics in cand_news_topics_group:
+            news_score = topic_scores.get(cand_topics[0], 0) + topic_scores.get(cand_topics[1], 0)
+            news_scores = numpy.append(news_scores, news_score)
+        
+        self.__logger.debug(f"Candidate news scores: {news_scores}")
+
+        highest_news = news_scores.argmax()
+
+        self.__logger.debug(f"Highest scored news index: {highest_news}")
+
+        news_predictions = numpy.zeros(len(news_scores))
+
+        news_predictions[highest_news] = 1
 
         return news_predictions

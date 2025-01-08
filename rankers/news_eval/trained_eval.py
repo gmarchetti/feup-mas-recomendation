2e8855c42bcde5b1db2aec0d691f94dfeb1f671f
Guilherme Marcetti <guilherme.marchetti@gmail.com>
2025-01-08 15:39:13 +0000

    added functions to support trained rankers

--- rankers/news_eval/trained_eval.py
+++ rankers/news_eval/trained_eval.py
@@ -8,13 +8,12 @@ from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
 
 class TrainedEval():
-    
+
     def preprocess_function(self, hist_titles, cand_title):
-        full_concat = ""
+        full_concat = cand_title.lower()
+
         for title in hist_titles:
-            full_concat += title.lower() + "[SEP]"
-        
-        full_concat += cand_title.lower()
+            full_concat += "[SEP]" + title.lower()        
 
         return self.__tokenizer(full_concat, truncation=True, return_tensors="pt")
     
@@ -23,13 +22,22 @@ class TrainedEval():
         self.__model = AutoModelForSequenceClassification.from_pretrained(model_path, num_labels=2)
         self.__logger = logging.getLogger(__name__)
 
-    def predict(self, hist_titles, cand_title):
+    def __get_prob_is_clicked(self, hist_titles, cand_title):
         with torch.no_grad():
             
             tokenized_sample = self.preprocess_function(hist_titles, cand_title)
             logits = self.__model(**tokenized_sample).logits[0][1].item()
 
             return logits
+
+    def __get_most_prob_label(self, hist_titles, cand_title):
+        with torch.no_grad():
+            
+            tokenized_sample = self.preprocess_function(hist_titles, cand_title)
+            logits = self.__model(**tokenized_sample).logits
+            predicted_class_id = logits.argmax().item()
+            return predicted_class_id
+
 
     def order_news(self, impression_data):
         
@@ -39,7 +47,7 @@ class TrainedEval():
         news_values_dict = {}
         idx = 0
         for cand_news_title in impression_data["candidate_titles"]:
-            news_values_dict[cand_news[idx]] = self.predict(impression_data["history_titles"], cand_news_title)
+            news_values_dict[cand_news[idx]] = self.__get_prob_is_clicked(impression_data["history_titles"], cand_news_title)
             idx += 1
         
         self.__logger.debug(">>> Candidate news values")
@@ -49,5 +57,20 @@ class TrainedEval():
         self.__logger.debug(ordered_news)
         return ordered_news
     
+    def predict_impression_group_labels(self, impression_data):
+        cand_news = impression_data["candidate_news_index"]
+        self.__logger.debug(">>> Candidate news indexes")
+        self.__logger.debug(cand_news)
+
+        predicted_labels = []
+        for cand_news_title in impression_data["candidate_titles"]:
+            predicted_labels.append(self.__get_most_prob_label(impression_data["history_titles"], cand_news_title))
+
+        
+        self.__logger.debug(">>> Candidate news values")
+        self.__logger.debug(predicted_labels)
+
+        return predicted_labels
+
     def eval_news_offer(self, news, round):
         return random.uniform(1,4) * len(news) * math.pow(self._gama, round)

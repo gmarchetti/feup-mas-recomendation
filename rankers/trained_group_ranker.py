e07d0d1c464d364503671879f0442f49bdead1c5
Guilherme Marcetti <guilherme.marchetti@gmail.com>
2025-01-08 14:48:48 +0000

    using trained model to predict impression group label

---
+++ rankers/trained_group_ranker.py
@@ -0,0 +1,23 @@
+import math
+import numpy
+import logging
+
+from rankers.ranker_base import RankerBase
+from rankers.news_eval.base_eval import BaseEval
+from rankers.news_eval.trained_eval import TrainedEval
+
+TOP_RANKS = 2
+
+class TrainedGroupRanker(RankerBase):
+
+    def __init__(self, iterator):
+        super().__init__(iterator)
+
+        self.__title_eval = TrainedEval("models/news-prediction/checkpoint-61485")
+        self.__logger = logging.getLogger(__name__)
+
+    def predict(self, impression_info):
+
+        news_predictions = self.__title_eval.predict_impression_group_labels(impression_info)
+
+        return news_predictions
